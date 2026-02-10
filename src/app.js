71bd9c52bf9fe260169883feb144f7a812b7709b
lublubuterbrodi <lublubuterbrodi@gmail.com>
2026-02-10 18:18:05 +0200

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,32 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+function main() {
+  const args = process.argv.slice(2);
+
+  if (args.length < 2) {
+    console.error('Two arguments required: source and destination');
+
+    return;
+  }
+
+  const [source, destination] = args;
+
+  const sourcePath = path.resolve(source);
+  const destinationPath = path.resolve(destination);
+
+  if (sourcePath === destinationPath) {
+    return;
+  }
+
+  try {
+    fs.copyFileSync(sourcePath, destinationPath);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+main();
